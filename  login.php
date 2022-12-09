acf354ba7e9db18af67dd2c8aa7fbe1a11fa817f
k11n14 <k11n14@icloud.com>
2022-12-09 17:37:35 +0900

    ログイン画面のhtml作成

---  login.php
+++  login.php
@@ -8,6 +8,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ログインページ</title>
 </head>
+
+<link rel="stylesheet" href="./css/php_chanpionship.css">
 <body>
   <form action="" method="">
     <h1>ログイン</h1>
