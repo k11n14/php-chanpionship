1af57c4db388efdb5f4da41920a3c983711dca55
k11n14 <k11n14@icloud.com>
2022-12-09 17:33:07 +0900

    first commit

---
+++  login.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログインページ</title>
+</head>
+<body>
+  <form action="" method="">
+    <h1>ログイン</h1>
+    <p>ID: <input type="text" name="login_id"></p>
+    <p>PASS: <input type="password" name="login_password"></p>
+  </form>
+  
+</body>
+</html>
