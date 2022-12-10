98bd6c5bde7f93c561f12ac95495b9cf579bde71
k11n14 <k11n14@icloud.com>
2022-12-10 18:00:12 +0900

    ログインIDとPASSが入力されてない場合、最初のページに戻す。s

--- login_server.php
+++ login_server.php
@@ -9,4 +9,14 @@ echo('<pre>');
 var_dump($_SESSION);
 echo('</pre>');
 
+if(
+  // !isset($_POST["login_id"]||$_POST["login_id"]==""
+    !isset($_POST["login_id"])||$_POST["login_id"]==""||!isset($_POST ["login_password"])||$_POST ["login_password"]==""
+  ){
+  $alert='<script>alert("ログインできません")</script>';
+    echo $alert;
+    echo '<script>location.href = "login.php"</script>';
+    //  login.php
+}
+
 ?>
