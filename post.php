5b66853f00064a69bf9f1ac5fecca831da397dc6
k11n14 <k11n14@icloud.com>
2022-12-14 15:10:09 +0900

    投稿画面だけ。最低限

---
+++ post.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>投稿画面</title>
+</head>
+<body>
+  <form action="Twitter_main_server.php" method="POST">
+  <fieldset>
+    <legend>投稿</legend>
+    <div>
+      一言: <input type="text" name="A_word">
+    </div>
+  </fieldset>
+</form>
+</body>
+</html>
