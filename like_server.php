036ecc4e8f439f8e6434322c5dfb2a10725a4d03
k11n14 <k11n14@icloud.com>
2022-12-15 13:07:32 +0900

    いいねボタンのonoffおk

--- like_server.php
+++ like_server.php
@@ -14,10 +14,41 @@ include('functions.php');
 $user_id = $_GET['user_id'];
 $post_id = $_GET["post_id"];
 
+echo $user_id;
+echo('<br>');
+echo $post_id;
+// ok
+// exit();
 $pdo = connect_db();
 
+$sql = 'SELECT COUNT(*) FROM Like_table WHERE post_id=:post_id AND users_id=:user_id';
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
+$stmt->bindValue(':post_id', $post_id, PDO::PARAM_STR);
+
+try {
+  $status = $stmt->execute();
+} catch (PDOException $e) {
+  echo json_encode(["sql error" => "{$e->getMessage()}"]);
+  exit();
+}
+
+$like_count = $stmt->fetchColumn();
+
+echo ('<pre>');
+var_dump($like_count);
+echo ('</pre>');
+// exit();
+
+if ($like_count !== '0') {
+  $sql = 'DELETE FROM Like_table WHERE post_id=:post_id AND users_id=:user_id';
+  echo 'DELETE';
+}
+else {
 $sql = 'INSERT INTO Like_table (like_id, post_id, users_id, like_created_at) 
-VALUES (NULL, :user_id, :post_id, now())';
+VALUES (NULL,:post_id,:user_id, now())';
+  echo 'INSERT';
+}
 
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':user_id', $user_id, PDO::PARAM_STR);
@@ -30,5 +61,6 @@ try {
   exit();
 }
 
-
+// header("Location:main.php");
+exit();
 ?>
