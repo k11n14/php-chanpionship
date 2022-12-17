7828d6c76dd6fe293c4e1351307d612d6eacc14c
k11n14 <k11n14@icloud.com>
2022-12-17 20:18:36 +0900

    JSの関数ファイル作成

--- JS/php_Campionship.js
+++ JS/php_Campionship.js
@@ -18,9 +18,6 @@ console.log(like_count.textContent);
 // const like_canvas = like_count.textContent;
 // console.log(like_canvas);
 
-
-
-
 // const canvas = document.getElementById("Post_canvas");
 const canvas = document.getElementsByClassName("post_canvas");
 
@@ -28,7 +25,7 @@ canvas.width = 120;
 canvas.height = 120;
 
 // if (canvas.getContext) {
-const context = canvas.getContext("2d");
+// const context = canvas.getContext("2d");
 //左から20上から40の位置に、幅50高さ100の四角形を描く
 // context.fillRect(canvas.width / 2, canvas.height / 2, 20, 20);
 
@@ -37,19 +34,19 @@ const context = canvas.getContext("2d");
 // 開始角度: 0度 (0 * Math.PI / 180)
 // 終了角度: 360度 (360 * Math.PI / 180)
 // 方向: true=反時計回りの円、false=時計回りの円
-context.arc(
-	canvas.width / 2,
-	canvas.height / 2,
-	like_count.textContent,
-	(0 * Math.PI) / 180,
-	(360 * Math.PI) / 180,
-	false
-);
+// context.arc(
+// 	canvas.width / 2,
+// 	canvas.height / 2,
+// 	like_count.textContent,
+// 	(0 * Math.PI) / 180,
+// 	(360 * Math.PI) / 180,
+// 	false
+// );
 // 塗りつぶしの色
-context.fillStyle = "red";
+// context.fillStyle = "red";
 
 // 塗りつぶしを実行
-context.fill();
+// context.fill();
 
 // 線の色
 // context.strokeStyle = "purple";
@@ -60,6 +57,6 @@ context.fill();
 // 線を描画を実行
 // context.stroke();
 
-context.strokeStyle = "rgb(00,00,255)";
-context.fillStyle = "rgb(255,00,00)";
+// context.strokeStyle = "rgb(00,00,255)";
+// context.fillStyle = "rgb(255,00,00)";
 // }
--- main.php
+++ main.php
@@ -21,7 +21,7 @@ include("main_server.php");
   </div>
   <div class="output"><?= $output ?></div>
   
-  <script src="./JS/main.js"></script>
+  <script src="./JS/php_Campionship.js"></script>
 </body>
 
 </html>
